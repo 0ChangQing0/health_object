73e8afca1ab57be939f5c0a9a13a26905eeb9eca
juzhao <375006562@qq.com>
2020-10-18 18:23:03 +0800

    提交ssm_parent项目

---
+++ ssm_parent/ssm_service/src/main/java/com/itheima/service/Impl/ItemsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.itheima.service.Impl;
+
+import com.itheima.daoTest.ItemsDao;
+import com.itheima.pojo.Items;
+import com.itheima.service.ItemsService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ItemsServiceImpl implements ItemsService {
+    @Autowired
+    private ItemsDao itemsDao;
+
+    /***
+     * 集合查询
+     * @return
+     */
+    public List<Items> findAll() {
+        return itemsDao.findAll();
+    }
+
+    /***
+     * 增加商品测试事务
+     * @param items
+     * @return
+     */
+    public int save(Items items) {
+        int acount = itemsDao.save(items);
+
+        System.out.println("acount:"+acount);
+
+        //测试事务，如果出错，是否回滚
+        //int q=10/0;
+
+        return acount;
+    }
+}
---
+++ ssm_parent/ssm_service/src/test/java/com/itheima/serviceTest/ServiceTest.java
@@ -0,0 +1,15 @@
+package com.itheima.serviceTest;
+
+import com.itheima.pojo.Items;
+import com.itheima.service.ItemsService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ServiceTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-service.xml");
+        ItemsService itemsService = (ItemsService)applicationContext.getBean("itemsServiceImpl");
+        Items items = new Items();
+        items.setName("测试事务");
+        itemsService.save(items);
+    }
+}
